44e0e37b02f969e7b8857e1cd6fead6320d5d608
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-10-23 10:39:46 +0200

    Initialize the project

---
+++ README.md
@@ -0,0 +1,8 @@
+[![Mergify Status][mergify-status]][mergify]
+
+[mergify]: https://mergify.io
+[mergify-status]: https://img.shields.io/endpoint.svg?url=https://gh.mergify.io/badges/Mergifyio/react-crisp&style=flat
+
+# React-Crisp
+
+React implementation for the messaging application [Crisp](https://crisp.chat/)
---
+++ src/Crisp.jsx
@@ -0,0 +1,82 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+function insertScript() {
+  const script = document.createElement('script');
+
+  script.src = 'https://client.crisp.chat/l.js';
+  script.async = 1;
+
+  document.head.appendChild(script);
+}
+class Crisp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.setUpCrisp = this.setUpCrisp.bind(this);
+    this.setAttributes = this.setAttributes.bind(this);
+    this.setSafeMode = this.setSafeMode.bind(this);
+  }
+
+  componentDidMount() {
+    this.setUpCrisp();
+    this.setAttributes();
+    this.safeMode();
+    insertScript();
+  }
+
+  setUpCrisp() {
+    const { crispWebsiteId, crispTokenId, crispRuntimeConfig } = this.props;
+
+    window.$crisp = [];
+
+    if (crispWebsiteId) {
+      window.CRISP_WEBSITE_ID = crispWebsiteId;
+    }
+    if (crispTokenId !== '') {
+      window.CRISP_TOKEN_ID = crispTokenId;
+    }
+    if (crispRuntimeConfig.length > 0) {
+      window.CRISP_RUNTIME_CONFIG = crispRuntimeConfig;
+    }
+  }
+
+  setAttributes() {
+    const { attributes } = this.props;
+
+    if (attributes !== {}) {
+      const categories = Object.entries(attributes);
+
+      categories.map((category) => {
+        const [key, value] = category;
+        return window.$crisp.push(['set', key, value]);
+      });
+    }
+  }
+
+  setSafeMode() {
+    const { safeMode } = this.props;
+    window.$crisp.push(['safe', safeMode]);
+  }
+
+  render() {
+    return (
+      <></>
+    );
+  }
+}
+Crisp.propTypes = {
+  crispWebsiteId: PropTypes.string.isRequired,
+  crispTokenId: PropTypes.string,
+  crispRuntimeConfig: PropTypes.objectOf(PropTypes.element),
+  attributes: PropTypes.objectOf(PropTypes.node),
+  safeMode: PropTypes.bool,
+};
+Crisp.defaultProps = {
+  crispTokenId: '',
+  crispRuntimeConfig: {},
+  attributes: {},
+  safeMode: false,
+};
+
+export default Crisp;
