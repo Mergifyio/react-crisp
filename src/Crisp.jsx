ee9ba9a677258c69757a3ecc05be33943037e89d
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2020-11-18 15:10:44 +0100

    refactor: refactor Crisp and tests
    - Move setUpCrisp's content in componentDidMount
    - Merge setAttributes and setConfiguration in one method configCrisp
    - Use configCrisp in componentDidUpdate instead of componentDidMount
    - Delete tests on configCrisp since its not possible to perform them
      without a real websiteId

--- src/Crisp.jsx
+++ src/Crisp.jsx
@@ -2,84 +2,76 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 function insertScript() {
-  const script = document.createElement('script');
+  const scriptUrl = 'https://client.crisp.chat/l.js';
+  const scripts = document.querySelector(`script[src='${scriptUrl}']`);
+  if (scripts === null) {
+    const script = document.createElement('script');
 
-  script.src = 'https://client.crisp.chat/l.js';
-  script.async = 1;
+    script.src = scriptUrl;
+    script.async = 1;
 
-  document.head.appendChild(script);
+    document.head.appendChild(script);
+  }
 }
+
+function pushCrisp(method, parameters) {
+  if (Object.keys(parameters).length > 0) {
+    const items = Object.entries(parameters);
+    items.map((item) => {
+      const [key, value] = item;
+      return global.$crisp.push(['set', key, value]);
+    });
+  }
+}
+
 class Crisp extends React.Component {
   constructor(props) {
     super(props);
 
-    this.setUpCrisp = this.setUpCrisp.bind(this);
-    this.setAttributes = this.setAttributes.bind(this);
-    this.setConfiguration = this.setConfiguration.bind(this);
-    this.setSafeMode = this.setSafeMode.bind(this);
+    this.configCrisp = this.configCrisp.bind(this);
   }
 
   componentDidMount() {
-    this.setUpCrisp();
-    this.setAttributes();
-    this.setConfiguration();
-    this.setSafeMode();
+    const {
+      crispWebsiteId, crispTokenId, crispRuntimeConfig, safeMode,
+    } = this.props;
+
+    global.$crisp = [];
+
+    // CRISP_WEBSITE_ID, CRISP_TOKEN_ID and CRISP_RUNTIME_CONFIG
+    // must be declared before inserting the script
+    // https://help.crisp.chat/en/article/how-to-restore-chat-sessions-with-a-token-c32v4t/
+    // https://help.crisp.chat/en/article/how-to-use-crisp-with-reactjs-fe0eyz/
+
+    global.CRISP_WEBSITE_ID = crispWebsiteId;
+
+    global.CRISP_TOKEN_ID = crispTokenId;
+
+    global.CRISP_RUNTIME_CONFIG = crispRuntimeConfig;
+
     insertScript();
+
+    // Must be call before any other $crisp method
+    // https://help.crisp.chat/en/article/how-to-use-dollarcrisp-javascript-sdk-10ud15y/#1-disable-warnings-amp-errors
+    global.$crisp.push(['safe', safeMode]);
   }
 
-  setUpCrisp() {
-    const { crispWebsiteId, crispTokenId, crispRuntimeConfig } = this.props;
-
-    window.$crisp = [];
-
-    if (crispWebsiteId) {
-      window.CRISP_WEBSITE_ID = crispWebsiteId;
-    }
-    if (crispTokenId !== '') {
-      window.CRISP_TOKEN_ID = crispTokenId;
-    }
-    if (Object.keys(crispRuntimeConfig).length > 0) {
-      window.CRISP_RUNTIME_CONFIG = crispRuntimeConfig;
-    }
+  componentDidUpdate() {
+    this.configCrisp();
   }
 
-  setAttributes() {
-    const { attributes } = this.props;
+  configCrisp() {
+    const { configuration, attributes } = this.props;
 
-    if (Object.keys(attributes).length > 0) {
-      const categories = Object.entries(attributes);
-
-      categories.map((category) => {
-        const [key, value] = category;
-        return window.$crisp.push(['set', key, value]);
-      });
-    }
-  }
-
-  setConfiguration() {
-    const { configuration } = this.props;
-
-    if (Object.keys(configuration).length > 0) {
-      const parameters = Object.entries(configuration);
-
-      parameters.map((parameter) => {
-        const [key, value] = parameter;
-        return window.$crisp.push(['config', key, value]);
-      });
-    }
-  }
-
-  setSafeMode() {
-    const { safeMode } = this.props;
-    window.$crisp.push(['safe', safeMode]);
+    pushCrisp('set', attributes);
+    pushCrisp('config', configuration);
   }
 
   render() {
-    return (
-      ''
-    );
+    return <></>;
   }
 }
+
 Crisp.propTypes = {
   crispWebsiteId: PropTypes.string.isRequired,
   crispTokenId: PropTypes.string,
