89e7c6f6224c5718656870b2014cf84f60de54c9
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2021-04-06 12:22:50 +0200

    chore: build the package with another target
    As it was asked in https://github.com/Mergifyio/react-crisp/issues/39
    This add a new target for building the package.

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,45 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/Crisp.jsx',
-  output: {
-    path: path.resolve('dist'),
-    filename: 'crisp.js',
-    libraryTarget: 'commonjs2',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: 'babel-loader',
+module.exports = [
+  {
+    entry: './src/Crisp.jsx',
+    output: {
+      path: path.resolve(__dirname, './dist/web/'),
+      filename: 'crisp-web.js',
+      library: {
+        type: 'commonjs2',
       },
-    ],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules)/,
+          use: 'babel-loader',
+        },
+      ],
+    },
   },
-};
+  {
+    entry: './src/Crisp.jsx',
+    experiments: {
+      outputModule: true,
+    },
+    output: {
+      path: path.resolve(__dirname, './dist/module/'),
+      filename: 'crisp-module.js',
+      library: {
+        type: 'module',
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules)/,
+          use: 'babel-loader',
+        },
+      ],
+    },
+  },
+];
